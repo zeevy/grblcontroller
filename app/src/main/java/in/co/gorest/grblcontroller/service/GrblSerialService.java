d5bcf88565e94f677fd0288c1fcd2223b097b572
zeevy <zeevy@outlook.in>
2018-01-07 22:15:00 +0530

    disconnect service on destroy

--- app/src/main/java/in/co/gorest/grblcontroller/service/GrblSerialService.java
+++ app/src/main/java/in/co/gorest/grblcontroller/service/GrblSerialService.java
@@ -102,6 +102,7 @@ public class GrblSerialService extends SerialThreadService{
 
     @Override
     public void onDestroy(){
+        disconnectService();
         EventBus.getDefault().unregister(this);
         super.onDestroy();
     }
