ab9a74b42ec16109eefd7bd20e9a3ec2dc7e4ed3
zeevy <zeevy@outlook.in>
2018-02-06 16:15:40 +0530

    Added number picker preference

---
+++ app/src/main/java/in/co/gorest/grblcontroller/helpers/NumberPickerPreference.java
@@ -0,0 +1,108 @@
+/*
+ *
+ *  *  /**
+ *  *  * Copyright (C) 2017  Grbl Controller Contributors
+ *  *  *
+ *  *  * This program is free software; you can redistribute it and/or modify
+ *  *  * it under the terms of the GNU General Public License as published by
+ *  *  * the Free Software Foundation; either version 2 of the License, or
+ *  *  * (at your option) any later version.
+ *  *  *
+ *  *  * This program is distributed in the hope that it will be useful,
+ *  *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  *  * GNU General Public License for more details.
+ *  *  *
+ *  *  * You should have received a copy of the GNU General Public License along
+ *  *  * with this program; if not, write to the Free Software Foundation, Inc.,
+ *  *  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *  *  * <http://www.gnu.org/licenses/>
+ *  *
+ *
+ */
+
+package in.co.gorest.grblcontroller.helpers;
+
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.preference.DialogPreference;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.NumberPicker;
+
+public class NumberPickerPreference extends DialogPreference {
+
+    public static final int MAX_VALUE = 100;
+    public static final int MIN_VALUE = 1;
+
+    public static final boolean WRAP_SELECTOR_WHEEL = true;
+
+    private NumberPicker picker;
+    private int value;
+
+    public NumberPickerPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public NumberPickerPreference(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected View onCreateDialogView() {
+        FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutParams.gravity = Gravity.CENTER;
+
+        picker = new NumberPicker(getContext());
+        picker.setLayoutParams(layoutParams);
+
+        FrameLayout dialogView = new FrameLayout(getContext());
+        dialogView.addView(picker);
+
+        return dialogView;
+    }
+
+    @Override
+    protected void onBindDialogView(View view) {
+        super.onBindDialogView(view);
+        picker.setMinValue(MIN_VALUE);
+        picker.setMaxValue(MAX_VALUE);
+        picker.setWrapSelectorWheel(WRAP_SELECTOR_WHEEL);
+        picker.setValue(getValue());
+    }
+
+    @Override
+    protected void onDialogClosed(boolean positiveResult) {
+        if (positiveResult) {
+            picker.clearFocus();
+            int newValue = picker.getValue();
+            if (callChangeListener(newValue)) {
+                setValue(newValue);
+            }
+        }
+    }
+
+    @Override
+    protected Object onGetDefaultValue(TypedArray a, int index) {
+        return a.getInt(index, MIN_VALUE);
+    }
+
+    @Override
+    protected void onSetInitialValue(boolean restorePersistedValue, Object defaultValue) {
+        setValue(restorePersistedValue ? getPersistedInt(MIN_VALUE) : (Integer) defaultValue);
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+        persistInt(this.value);
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+}
