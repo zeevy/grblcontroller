3230f09e91ddbb209f076afa209734cd61668cca
zeevy <zeevy@outlook.in>
2018-04-19 15:25:41 +0530

    added Smoothie board support

--- app/src/main/java/in/co/gorest/grblcontroller/util/GrblUtils.java
+++ app/src/main/java/in/co/gorest/grblcontroller/util/GrblUtils.java
@@ -63,7 +63,7 @@ public class GrblUtils {
     }
 
     public static Boolean isSmoothieBoard(final String response){
-        return response.toLowerCase().startsWith("Smoothie");
+        return response.toLowerCase().startsWith("smoothie");
     }
 
     private final static String VERSION_DOUBLE_REGEX = "[0-9]*\\.[0-9]*";
